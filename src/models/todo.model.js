c587727983012b4952abae0614d7f6664b53f9a0
ibrahem <ibrahem.reiad@gmail.com>
2020-02-23 00:17:56 +0200

    add some validation for todo Schema

--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose"),
   schema = mongoose.Schema,
   types = schema.Types;
 const todoSchema = new schema({
-  title: { type: types.String },
-  subject: { type: types.String },
-  completed: { type: types.Boolean },
+  title: { type: types.String, required: true },
+  subject: { type: types.String, required: true },
+  completed: { type: types.Boolean, default: false },
   date: { type: types.Number, default: Date.now },
   userId: { type: types.ObjectId, ref: "user" }
 });
